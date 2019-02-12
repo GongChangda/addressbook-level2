b4d296796cfce608ea5683a0a16e6ff17864068d
DESKTOP-BFINHE4\Gong changda <changda1998@gmail.com>
2019-02-12 21:53:06 +0800

    Modify the input.txt to make the format correct and also add one JUnit test

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -1,7 +1,49 @@
 package seedu.addressbook.commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.util.TestUtil;
 import static org.junit.Assert.*;
 
 public class SortCommandTest {
+    private AddressBook addressBook;
+    private String testResult;
 
+    @Test
+    public void sortTest() throws Exception{
+        Person johnDoe = new Person(new Name("John Doe"), new Phone("61234567", false),
+                new Email("john@doe.com", false), new Address("395C Ben Road", false), Collections.emptySet());
+        Person janeDoe = new Person(new Name("Jane Doe"), new Phone("91234567", false),
+                new Email("jane@doe.com", false), new Address("33G Ohm Road", false), Collections.emptySet());
+        Person samDoe = new Person(new Name("Sam Doe"), new Phone("63345566", false),
+                new Email("sam@doe.com", false), new Address("55G Abc Road", false), Collections.emptySet());
+        Person davidGrant = new Person(new Name("David Grant"), new Phone("61121122", false),
+                new Email("david@grant.com", false), new Address("44H Define Road", false),
+                Collections.emptySet());
+
+        addressBook = TestUtil.createAddressBook(johnDoe, janeDoe, davidGrant, samDoe);
+        addressBook.sort();
+        testResult = addressBook.getAllPersons().immutableListView().get(0).getName().fullName;
+        assertTrue(testResult.equals("David Grant"));
+    }
 }
